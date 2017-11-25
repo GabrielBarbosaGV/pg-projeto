bc5b0ff646b0167314124afe9b438f6a9bc544e0
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2017-11-25 20:56:56 -0300

    Avanços

--- script.js
+++ script.js
@@ -1,5 +1,187 @@
-// FUNÇÕES PARA LEITURA DE ARQUIVOS
+// CONFIGURAÇÕES PRÉ-EXECUÇÃO
 
+/*
+Define novo método .last() para arrays que retorna o último elemento do array.
+Criado para selecionar o último elemento de um array resultante de uma chamada de método, sem que tenha de ser atribuído a uma variável temporária
+*/
+Array.prototype.last = () => {
+	return this[this.length - 1];
+}
+// FIM DE CONFIGURAÇÕES PRÉ-EXECUÇÃO
+
+
+
+// FUNÇÕES PARA LEITURA DE ARQUIVOS
+function hasNecessaryFiles() {
+	let hasObj = false, hasCam = false, hasIll = false,
+		inputs = document.getElementById('file-selection').getElementsByTagName('input');
+
+	for (let inputN in inputs) {
+		let extension = inputs[inputN].files[0].name.split('.').last();
+		
+		switch (extension) {
+			case 'byu':
+				hasObj = true;
+				break;
+			case 'cfg':
+				hasCam = true;
+				break;
+			case 'txt':
+				hasIll = true;
+				break;
+			default:
+				alert('Please insert valid files.');
+				return false;
+		}
+	}
+
+	return hasObj && hasCam && hasIll;
+}
+
+function readFile(evt) {
+	var files = evt.target.files;
+	var reader = new FileReader();
+
+	reader.onload = () => {
+		let extension = files[0].name.split('.').last();
+
+		switch (extension) {
+			case 'byu':
+				objectInfo = reader.result();
+				break;
+			case 'cfg':
+				cameraInfo = reader.result();
+				break;
+			case 'txt':
+				illuminationInfo = reader.result();
+				break;
+			default:
+				alert("Unrecognised file exension, please select a recognised filetype. (.byu, .cfg, .txt)");
+				break;
+		}
+	};
+
+	reader.readAsText(files[0]);
+}
+
+function interpretObjectInfo() {
+	var objectLines = objectInfo.split('\r\n');
+
+	var info = objectLines[0].split(' ');
+
+	for (let i in info) info[i] = parseFloat(info[i]);
+
+
+	var points = [];
+
+	for (let line = 1;line < info[0] + 1;line++) {
+		var point = objectLines[line + 1].split(' ');
+
+		for (let coord in point) point[coord] = parseFloat(point[coord]);
+
+		points.push(point);
+	}
+
+	var triangles = [];
+
+	for (let line = 1 + info[0];line < info[1] + 1 + info[0];line++) {
+		var triangle = objectLines[line].split(' ');
+
+		for (let i in triangle) triangle[i] = parseInt(triangle[i]) - 1;
+
+		triangles.push(triangle);
+	}
+
+	return {
+		points: points,
+		triangles: triangles
+	}
+}
+
+function interpretCameraInfo() {
+	var cameraLines = cameraInfo.split('\r\n');
+
+	var c = cameraLines[0].split(' ');
+
+	for (let coord in c) c[coord] = parseFloat(c[coord]);
+
+
+	var n = cameraLines[1].split(' ');
+
+	for (let coord in n) n[coord] = parseFloat(n[coord]);
+
+
+	var v = cameraLines[2].split(' ');
+
+	for (let coord in v) v[coord] = parseFloat(v[coord]);
+
+
+	var dhxhy = cameraLines[3].split(' ');
+
+	for (let val in dhxhy) dhxhy[val] = parseFloat(dhxhy[val]);
+
+	return {
+		c: c,
+		n: n,
+		v: v,
+		d: dhxhy[0],
+		hx: dhxhy[1],
+		hy: dhxhy[2]
+	}
+}
+
+function interpretIlluminationInfo() {
+	var illuminationLines = illuminationInfo.split('\r\n');
+
+	var pl = illuminationInfo[0].split(' ');
+
+	for (let i in pl) pl[i] = parseFloat(pl[i]);
+
+	var ka = parseFloat(illuminationInfo[1]);
+
+
+	var ia = illuminationInfo[2].split(' ');
+
+	for (let i in ia) ia[i] = parseFloat(ia[i]);
+
+
+	var kd = parseFloat(illuminationInfo[3]);
+
+
+	var od = illuminationInfo[4].split(' ');
+
+	for (let i in od) od[i] = parseFloat(od[i]);
+
+
+	var ks = parseFloat(illuminationInfo[5]);
+
+
+	var il = illuminationInfo[6].split(' ');
+
+	for (let i in il) il[i] = parseInt(il[i]);
+
+
+	var n = parseFloat(illuminationInfo[7]);
+
+	return {
+		pl: pl,
+		ka: ka,
+		ia: ia,
+		kd: kd,
+		od: od,
+		ks: ks,
+		il: il,
+		n: n
+	}
+}
+
+function interpretData(evt) {
+	if (hasNecessaryFiles()) {
+		camera = interpretCameraInfo();
+		object = interpretObjectInfo();
+		illumination = interpretIlluminationInfo();
+	} else alert('Please input valid files.');
+}
 // FIM DE FUNÇÕES PARA LEITURA DE ARQUIVOS
 
 
@@ -30,15 +212,19 @@ function gramSchmidt(toChange, referenceVector) {
 }
 // FIM DE FUNÇÕES PARA VETORES
 
-var camera;
 
-function readFile(evt) {
-  var files = evt.target.files;
-  var reader = new FileReader();
-  reader.onload = function() {
-    camera = reader.result;
-  };
-  reader.readAsText(files[0]);
+
+//EXECUÇÃO:
+var  object, camera, illumination,
+	objectInfo, cameraInfo, illuminationInfo;
+
+window.onload = () => {
+	let inputs = document.getElementById('file-selection').getElementsByTagName('input');
+
+	for (let inputN in inputs) {
+		document.getElementById(inputs[inputN].id).addEventListener('change', readFile, false);
+	}
+
+
+	document.getElementById('read').addEventListener('click', interpretData, false);
 }
-
-document.getElementById('file1').addEventListener('change', readFile, false);
