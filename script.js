3009376e2cc82b6375772c92cf08b69658384b7e
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2017-11-28 22:09:39 -0300

    Ajustes.

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function interpretObjectInfo() {
 	var points = [];
 
 	for (let line = 1;line < info[0] + 1;line++) {
-		var point = objectLines[line + 1].split(' '), normal = [];
+		var point = objectLines[line + 1].split(' ').filter(i => i), normal = [];
 
 		for (let coord in point) {
 		       	point[coord] = parseFloat(point[coord]);
@@ -82,7 +82,7 @@ function interpretObjectInfo() {
 	var triangles = [];
 
 	for (let line = 1 + info[0];line < info[1] + 1 + info[0];line++) {
-		var triangle = objectLines[line].split(' ');
+		var triangle = objectLines[line].split(' ').filter(i => i);
 
 		for (let i in triangle) triangle[i] = parseInt(triangle[i]) - 1;
 
@@ -110,22 +110,22 @@ function interpretObjectInfo() {
 function interpretCameraInfo() {
 	var cameraLines = cameraInfo.split(/\r\n/).filter(i => i);
 
-	var c = cameraLines[0].split(' ');
+	var c = cameraLines[0].split(' ').filter(i => i);
 
 	for (let coord in c) c[coord] = parseFloat(c[coord]);
 
 
-	var n = cameraLines[1].split(' ');
+	var n = cameraLines[1].split(' ').filter(i => i);
 
 	for (let coord in n) n[coord] = parseFloat(n[coord]);
 
 
-	var v = cameraLines[2].split(' ');
+	var v = cameraLines[2].split(' ').filter(i => i);
 
 	for (let coord in v) v[coord] = parseFloat(v[coord]);
 
 
-	var dhxhy = cameraLines[3].split(' ');
+	var dhxhy = cameraLines[3].split(' ').filter(i => i);
 
 	for (let val in dhxhy) dhxhy[val] = parseFloat(dhxhy[val]);
 
@@ -143,14 +143,14 @@ function interpretCameraInfo() {
 function interpretIlluminationInfo() {
 	var illuminationLines = illuminationInfo.split(/\r\n/).filter(i => i);
 
-	var pl = illuminationInfo[0].split(' ');
+	var pl = illuminationInfo[0].split(' ').filter(i => i);
 
 	for (let i in pl) pl[i] = parseFloat(pl[i]);
 
 	var ka = parseFloat(illuminationInfo[1]);
 
 
-	var ia = illuminationInfo[2].split(' ');
+	var ia = illuminationInfo[2].split(' ').filter(i => i);
 
 	for (let i in ia) ia[i] = parseFloat(ia[i]);
 
@@ -158,7 +158,7 @@ function interpretIlluminationInfo() {
 	var kd = parseFloat(illuminationInfo[3]);
 
 
-	var od = illuminationInfo[4].split(' ');
+	var od = illuminationInfo[4].split(' ').filter(i => i);
 
 	for (let i in od) od[i] = parseFloat(od[i]);
 
@@ -166,7 +166,7 @@ function interpretIlluminationInfo() {
 	var ks = parseFloat(illuminationInfo[5]);
 
 
-	var il = illuminationInfo[6].split(' ');
+	var il = illuminationInfo[6].split(' ').filter(i => i);
 
 	for (let i in il) il[i] = parseInt(il[i]);
 
@@ -191,7 +191,7 @@ function interpretData(evt) {
 		object = interpretObjectInfo();
 		illumination = interpretIlluminationInfo();
 
-		camera.v = gramSchmidt(camera.v, camera.n);
+		camera.v = normalizeVector(gramSchmidt(camera.v, camera.n));
 		camera.u = vectorProduct(camera.n, camera.v);
 	} else alert('Please input valid files.');
 }
@@ -222,7 +222,7 @@ function vectorSum(vectorA, vectorB) {
 function vectorSubtraction(vectorA, toSubtract) {
 	if (vectorA.length === toSubtract.length) {
 		var subtractionVector = [];
-		for (let coordN in vectorA) subractionVector.push(vectorA[coordN] - toSubtract[coordN]);
+		for (let coordN in vectorA) subtractionVector.push(vectorA[coordN] - toSubtract[coordN]);
 		
 		return subtractionVector;
 	} else return NaN;
@@ -246,13 +246,13 @@ function normalizeVector(vector) {
 }
 
 function vectorNorm(vector) {
-	var normalVector = 0;
+	var norm = 0;
 
 	for (let coordN in vector) {
-		versor += Math.pow(vector[coordN], 2);
+		norm += Math.pow(vector[coordN], 2);
 	}
 
-	return Math.sqrt(versor);
+	return Math.sqrt(norm);
 }
 
 function projectVector(toProject, referenceVector) {
@@ -265,11 +265,11 @@ function projectVector(toProject, referenceVector) {
 }
 
 function gramSchmidt(toChange, referenceVector) {
-	return toChange - projectVector(toChange, referenceVector);
+	return vectorSubtraction(toChange, projectVector(toChange, referenceVector));
 }
 
 function vectorProduct(vectorA, vectorB) {
-	if (vectorA.length === vectorB.length === 3) {
+	if ((vectorA.length === 3) && (vectorB.length === 3)) {
 		var vectorProduct = [];
 
 		vectorProduct.push(vectorA[1]*vectorB[2] - vectorA[2]*vectorB[1]);
